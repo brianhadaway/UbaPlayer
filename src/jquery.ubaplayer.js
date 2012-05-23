654ede29d0b7a3f09b31d422119dd89e78c4b01e
Brian Hadaway <brianhadaway@gmail.com>
2012-05-23 18:21:17 -0500

    Test crossdomain

--- src/jquery.ubaplayer.js
+++ src/jquery.ubaplayer.js
@@ -45,6 +45,7 @@
 				if(audio) {
 					_methods.removeListeners(window);
 				}
+				console.log( 'defaults.flashObjectID', defaults.flashObjectID );
 				audio = document.getElementById(defaults.flashObjectID);
 				_methods.addListeners(window);
 				audio.playFlash(currentTrack + defaults.extension);
