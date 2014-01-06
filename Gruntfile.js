306797ce4103f07d1f421fc91b949f98bf2b5029
Brian Hadaway <brianhadaway@gmail.com>
2014-01-06 11:28:33 -0800

    More changes to support Grunt 0.4.
    Grunt file was not properly updated to support removal of default Grunt
    tasks in commit 838b845.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,60 +1,76 @@
 /*global module:false*/
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: '<json:package.json>',
-    meta: {
-      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-        '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
-        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
-    },
-    concat: {
-      dist: {
-        src: ['<banner:meta.banner>', '<file_strip_banner:src/<%= pkg.name %>.js>'],
-        dest: 'dist/<%= pkg.name %>.js'
-      }
-    },
-    min: {
-      dist: {
-        src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
-        dest: 'dist/<%= pkg.name %>.min.js'
-      }
-    },
-    qunit: {
-      files: ['test/**/*.html']
-    },
-    lint: {
-      files: ['grunt.js', 'src/**/*.js', 'test/**/*.js']
-    },
-    watch: {
-      files: '<config:lint.files>',
-      tasks: 'lint qunit'
-    },
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        boss: true,
-        eqnull: true,
-        browser: true
-      },
-      globals: {
-        jQuery: true
-      }
-    },
-    uglify: {}
-  });
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        meta: {
+            banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+                '<%= pkg.homepage ? " * " + pkg.homepage + "\\n " : "" %>' +
+                '* Copyright (c) <%= grunt.template.today(" yyyy ") %> <%= pkg.author.name %>;' +
+                ' Licensed <%= pkg.license.type %> */'
+        },
+        concat: {
+            options: {
+                separator: ';',
+                banner: '<%= meta.banner %>'
+            },
+            dist: {
+                src: ['src/jquery.ubaplayer.js'],
+                dest: 'dist/<%= pkg.name %>.js'
+            }
+        },
+        qunit: {
+            files: ['test/**/*.html']
+        },
+        watch: {
+            files: 'src/jquery.ubaplayer.js',
+            tasks: ['jshint']
+        },
+        jshint: {
+            files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+            options: {
+                curly: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                boss: true,
+                eqnull: true,
+                browser: true,
+                globals: {
+                    jQuery: true,
+                    $: true,
+                    swfobject: true
+                }
+            },
+        },
+        uglify: {
+            options: {
+                banner: '<%= meta.banner %>'
+            },
+            dist: {
+                files: {
+                    'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+                }
+            }
+        }
+    });
 
-  // Default task.
-  grunt.registerTask('default', 'lint qunit concat min');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task.
+    grunt.registerTask('default', [
+        'jshint',
+        //'qunit',
+        'concat',
+        'uglify'
+    ]);
 
 };
--- dist/jquery.ubaplayer.js
+++ dist/jquery.ubaplayer.js
@@ -1,255 +1,254 @@
-/*! UbaPlayer - v1.0.1 - 2013-07-22
-* https://github.com/brianhadaway/UbaPlayer
-* Copyright (c) 2013 Brian Hadaway; Licensed MIT, GPL */
+/*! UbaPlayer - v1.0.2 -  * https://github.com/brianhadaway/UbaPlayer
+ * Copyright (c)  2014  Brian Hadaway; Licensed MIT */(function($) {
+    var defaults = {
+        audioButtonClass: "audioButton",
+        autoPlay: null,
+        codecs: [{
+            name: "OGG",
+            codec: 'audio/ogg; codecs="vorbis"'
+        }, {
+            name: "MP3",
+            codec: 'audio/mpeg'
+        }],
+        continuous: false,
+        extension: null,
+        flashAudioPlayerPath: "libs/swf/player.swf",
+        flashExtension: ".mp3",
+        flashObjectID: "audioPlayer",
+        loadingClass: "loading",
+        loop: false,
+        playerContainer: "player",
+        playingClass: "playing",
+        swfobjectPath: "libs/swfobject/swfobject.js",
+        volume: 0.5,
+        fallbackFunctions: {
+            play: null,
+            pause: null,
+            resume: null,
+            error: null
+        },
+        fallbackExtension: '.mp3'
+    },
+        currentTrack, isPlaying = false,
+        isFlash = false,
+        isFallback = false,
+        audio, $buttons, $tgt, $el, playTrack, resumeTrack, pauseTrack, methods = {
+            play: function(element) {
+                $tgt = element;
+                currentTrack = _methods.getFileNameWithoutExtension($tgt.attr("href"));
+                isPlaying = true;
+                $tgt.addClass(defaults.loadingClass);
+                $buttons.removeClass(defaults.playingClass);
 
-(function($) {
-	var defaults = {
-		audioButtonClass: "audioButton",
-		autoPlay: null,
-		codecs: [{
-			name: "OGG",
-			codec: 'audio/ogg; codecs="vorbis"'
-		}, {
-			name: "MP3",
-			codec: 'audio/mpeg'
-		}],
-		continuous: false,
-		extension: null,
-		flashAudioPlayerPath: "libs/swf/player.swf",
-		flashExtension: ".mp3",
-		flashObjectID: "audioPlayer",
-		loadingClass: "loading",
-		loop: false,
-		playerContainer: "player",
-		playingClass: "playing",
-		swfobjectPath: "libs/swfobject/swfobject.js",
-		volume: 0.5,
-		fallbackFunctions: { play: null, pause: null, resume: null, error: null },
-		fallbackExtension: '.mid'
-	},
-		currentTrack, isPlaying = false,
-		isFlash = false,
-		isFallback = false,
-		audio, $buttons, $tgt, $el, playTrack, resumeTrack, pauseTrack, methods = {
-			play: function(element) {
-				$tgt = element;
-				currentTrack = _methods.getFileNameWithoutExtension($tgt.attr("href"));
-				isPlaying = true;
-				$tgt.addClass(defaults.loadingClass);
-				$buttons.removeClass(defaults.playingClass);
+                if (isFallback) {
+                    if (typeof defaults.fallbackFunctions.play == "function") {
+                        defaults.fallbackFunctions.play(currentTrack + defaults.fallbackExtension);
+                    }
+                } else if (isFlash) {
+                    if (audio) {
+                        _methods.removeListeners(window);
+                    }
+                    audio = document.getElementById(defaults.flashObjectID);
+                    _methods.addListeners(window);
+                    audio.playFlash(currentTrack + defaults.extension);
+                } else {
+                    if (audio) {
+                        audio.pause();
+                        _methods.removeListeners(audio);
+                    }
+                    audio = new Audio("");
+                    _methods.addListeners(audio);
+                    audio.id = "audio";
+                    audio.loop = defaults.loop ? "loop" : "";
+                    audio.volume = defaults.volume;
+                    audio.src = currentTrack + defaults.extension;
+                    audio.play();
+                }
+            },
 
-				if (isFallback) {
-					if (typeof defaults.fallbackFunctions.play == "function") {
-						defaults.fallbackFunctions.play(currentTrack + defaults.fallbackExtension);
-					}
-				}
-				else if (isFlash) {
-					if (audio) {
-						_methods.removeListeners(window);
-					}
-					audio = document.getElementById(defaults.flashObjectID);
-					_methods.addListeners(window);
-					audio.playFlash(currentTrack + defaults.extension);
-				} else {
-					if (audio) {
-						audio.pause();
-						_methods.removeListeners(audio);
-					}
-					audio = new Audio("");
-					_methods.addListeners(audio);
-					audio.id = "audio";
-					audio.loop = defaults.loop ? "loop" : "";
-					audio.volume = defaults.volume;
-					audio.src = currentTrack + defaults.extension;
-					audio.play();
-				}
-			},
+            pause: function() {
+                if (isFallback) {
+                    if (typeof defaults.fallbackFunctions.pause == "function") {
+                        defaults.fallbackFunctions.pause();
+                    }
+                } else if (isFlash) {
+                    audio.pauseFlash();
+                } else {
+                    audio.pause();
+                }
 
-			pause: function () {
-				if (isFallback) {
-					if (typeof defaults.fallbackFunctions.pause == "function") {
-						defaults.fallbackFunctions.pause();
-					}
-				}
-				else if (isFlash) {
-					audio.pauseFlash();
-				} else {
-					audio.pause();
-				}
+                $tgt.removeClass(defaults.playingClass);
+                isPlaying = false;
+            },
 
-				$tgt.removeClass(defaults.playingClass);
-				isPlaying = false;
-			},
+            resume: function() {
+                if (isFallback) {
+                    if (typeof defaults.fallbackFunctions.resume == "function") {
+                        defaults.fallbackFunctions.resume();
+                    }
+                } else if (isFlash) {
+                    audio.playFlash();
+                } else {
+                    audio.play();
+                }
+                $tgt.addClass(defaults.playingClass);
+                isPlaying = true;
+            },
 
-			resume: function () {
-				if (isFallback) {
-					if (typeof defaults.fallbackFunctions.resume == "function") {
-						defaults.fallbackFunctions.resume();
-					}
-				}
-				else if (isFlash) {
-					audio.playFlash();
-				} else {
-					audio.play();
-				}
-				$tgt.addClass(defaults.playingClass);
-				isPlaying = true;
-			},
+            playing: function() {
+                return isPlaying;
+            }
+        },
 
-			playing: function() {
-				return isPlaying;
-			}
-		},
+        _methods = {
+            init: function(options) {
+                var types;
 
-		_methods = {
-			init: function(options) {
-				var types;
+                //set defaults
+                $.extend(defaults, options);
+                $el = this;
 
-				//set defaults
-				$.extend(defaults, options);
-				$el = this;
+                //listen for clicks on the controls
+                $(".controls").bind("click", function(event) {
+                    _methods.updateTrackState(event);
+                    return false;
+                });
+                $buttons = $("." + defaults.audioButtonClass);
 
-				//listen for clicks on the controls
-				$(".controls").bind("click", function(event) {
-					_methods.updateTrackState(event);
-					return false;
-				});
-				$buttons = $("." + defaults.audioButtonClass);
+                types = defaults.codecs;
+                for (var i = 0, ilen = types.length; i < ilen; i++) {
+                    var type = types[i];
+                    if (_methods.canPlay(type)) {
+                        defaults.extension = [".", type.name.toLowerCase()].join("");
+                        break;
+                    }
+                }
 
-				types = defaults.codecs;
-				for (var i = 0, ilen = types.length; i < ilen; i++) {
-					var type = types[i];
-					if (_methods.canPlay(type)) {
-						defaults.extension = [".", type.name.toLowerCase()].join("");
-						break;
-					}
-				}
+                if (!defaults.extension || isFlash) {
+                    isFlash = true;
+                    defaults.extension = defaults.flashExtension;
+                }
 
-				if (!defaults.extension || isFlash) {
-					isFlash = true;
-					defaults.extension = defaults.flashExtension;
-				}
+                if (isFlash) {
+                    $el.html("<div id='" + defaults.playerContainer + "'/>");
+                    $.getScript(defaults.swfobjectPath, function() {
+                        swfobject.embedSWF(defaults.flashAudioPlayerPath, defaults.playerContainer, "0", "0", "9.0.0", "swf/expressInstall.swf", false, false, {
+                            id: defaults.flashObjectID
+                        }, _methods.swfLoaded);
+                    });
+                } else {
+                    if (defaults.autoPlay) {
+                        methods.play(defaults.autoPlay);
+                    }
+                }
+            },
 
-				if (isFlash) {
-					$el.html("<div id='" + defaults.playerContainer + "'/>");
-					$.getScript(defaults.swfobjectPath, function() {
-						swfobject.embedSWF(defaults.flashAudioPlayerPath, defaults.playerContainer, "0", "0", "9.0.0", "swf/expressInstall.swf", false, false, {
-							id: defaults.flashObjectID
-						}, _methods.swfLoaded);
-					});
-				} else {
-					if (defaults.autoPlay) {
-						methods.play(defaults.autoPlay);
-					}
-				}
-			},
+            updateTrackState: function(evt) {
+                $tgt = $(evt.target);
+                if (!$tgt.hasClass("audioButton")) {
+                    return;
+                }
+                if (!audio || (audio && currentTrack !== _methods.getFileNameWithoutExtension($tgt.attr("href")))) {
+                    methods.play($tgt);
+                } else if (!isPlaying) {
+                    methods.resume();
+                } else {
+                    methods.pause();
+                }
+            },
 
-			updateTrackState: function(evt) {
-				$tgt = $(evt.target);
-				if (!$tgt.hasClass("audioButton")) {
-					return;
-				}
-				if (!audio || (audio && currentTrack !== _methods.getFileNameWithoutExtension($tgt.attr("href")))) {
-					methods.play($tgt);
-				} else if (!isPlaying) {
-					methods.resume();
-				} else {
-					methods.pause();
-				}
-			},
+            addListeners: function(elem) {
+                $(elem).bind({
+                    "canplay": _methods.onLoaded,
+                    "error": _methods.onError,
+                    "ended": _methods.onEnded
+                });
+            },
 
-			addListeners: function(elem) {
-				$(elem).bind({
-					"canplay": _methods.onLoaded,
-					"error": _methods.onError,
-					"ended": _methods.onEnded
-				});
-			},
+            removeListeners: function(elem) {
+                $(elem).unbind({
+                    "canplay": _methods.onLoaded,
+                    "error": _methods.onError,
+                    "ended": _methods.onEnded
+                });
+            },
 
-			removeListeners: function(elem) {
-				$(elem).unbind({
-					"canplay": _methods.onLoaded,
-					"error": _methods.onError,
-					"ended": _methods.onEnded
-				});
-			},
+            onLoaded: function() {
+                $buttons.removeClass(defaults.loadingClass);
+                $tgt.addClass(defaults.playingClass);
 
-			onLoaded: function() {
-				$buttons.removeClass(defaults.loadingClass);
-				$tgt.addClass(defaults.playingClass);
+                audio.play();
+            },
 
-				audio.play();
-			},
+            onError: function() {
+                $buttons.removeClass(defaults.loadingClass);
+                if (isFlash) {
+                    _methods.removeListeners(window);
+                } else {
+                    _methods.removeListeners(audio);
+                }
+            },
 
-			onError: function() {
-				$buttons.removeClass(defaults.loadingClass);
-				if (isFlash) {
-					_methods.removeListeners(window);
-				} else {
-					_methods.removeListeners(audio);
-				}
-			},
+            onEnded: function() {
+                isPlaying = false;
+                $tgt.removeClass(defaults.playingClass);
+                currentTrack = "";
+                if (isFlash) {
+                    _methods.removeListeners(window);
+                } else {
+                    _methods.removeListeners(audio);
+                }
 
-			onEnded: function() {
-				isPlaying = false;
-				$tgt.removeClass(defaults.playingClass);
-				currentTrack = "";
-				if (isFlash) {
-					_methods.removeListeners(window);
-				} else {
-					_methods.removeListeners(audio);
-				}
+                if (defaults.continuous) {
+                    var $next = $tgt.next().length ? $tgt.next() : $(defaults.audioButtonClass).eq(0);
+                    methods.play($next);
+                }
 
-				if (defaults.continuous) {
-					var $next = $tgt.next().length ? $tgt.next() : $(defaults.audioButtonClass).eq(0);
-					methods.play($next);
-				}
+            },
 
-			},
+            canPlay: function(type) {
+                if (!document.createElement("audio").canPlayType) {
+                    return false;
+                } else {
+                    return document.createElement("audio").canPlayType(type.codec).match(/maybe|probably/i) ? true : false;
+                }
+            },
 
-			canPlay: function(type) {
-				if (!document.createElement("audio").canPlayType) {
-					return false;
-				} else {
-					return document.createElement("audio").canPlayType(type.codec).match(/maybe|probably/i) ? true : false;
-				}
-			},
+            swfLoaded: function(e) {
+                if (!e.success) {
+                    isFlash = false;
+                    isFallback = true;
 
-			swfLoaded: function (e) {
-				if (!e.success) {
-					isFlash = false;
-					isFallback = true;
+                    if (typeof defaults.fallbackFunctions.error == "function") {
+                        defaults.fallbackFunctions.error();
+                    }
+                }
 
-					if (typeof defaults.fallbackFunctions.error == "function") {
-						defaults.fallbackFunctions.error();
-					}
-				}
+                if (defaults.autoPlay) {
+                    setTimeout(function() {
+                        methods.play(defaults.autoPlay);
+                    }, 500);
+                }
+            },
 
-				if (defaults.autoPlay) {
-					setTimeout(function() {
-						methods.play(defaults.autoPlay);
-					}, 500);
-				}
-			},
+            getFileNameWithoutExtension: function(fileName) {
+                //this function take a full file name and returns an extensionless file name
+                //ex. entering foo.mp3 returns foo
+                //ex. entering foo returns foo (no change)
+                var fileNamePieces = fileName.split('.');
+                fileNamePieces.pop();
+                return fileNamePieces.join(".");
+            }
+        };
 
-			getFileNameWithoutExtension: function(fileName) {
-				//this function take a full file name and returns an extensionless file name
-				//ex. entering foo.mp3 returns foo
-				//ex. entering foo returns foo (no change)
-				var fileNamePieces = fileName.split('.');
-				fileNamePieces.pop();
-				return fileNamePieces.join(".");
-			}
-		};
-
-	$.fn.ubaPlayer = function(method) {
-		if (methods[method]) {
-			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-		} else if (typeof method === "object" || !method) {
-			return _methods.init.apply(this, arguments);
-		} else {
-			$.error("Method " + method + " does not exist on jquery.ubaPlayer");
-		}
-	};
+    $.fn.ubaPlayer = function(method) {
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === "object" || !method) {
+            return _methods.init.apply(this, arguments);
+        } else {
+            $.error("Method " + method + " does not exist on jquery.ubaPlayer");
+        }
+    };
 
 })(jQuery);
